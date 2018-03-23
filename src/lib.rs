b65c0f739b3107b624b9fe85c432e25d869edc9c
Alexander Ponimaskin <ponimaskin@gmail.com>
2018-03-23 12:07:09 +0300

    schema moved back to lib

--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,6 @@ extern crate serde_json;
 extern crate byteorder;
 extern crate rand;
 
-mod schema;
-
 /// Некоторый уникальный идентификатор сервиса.
 pub const CRYPTOOWLS_SERVICE_ID: u16 = 521;
 /// Уникальное имя сервиса, которое будет использоваться в апи и конфигурации.
@@ -146,6 +144,77 @@ mod data_layout {
 
 }
 
+// схема данных для базы
+mod schema {
+    use data_layout::{CryptoOwlState, Order, User};
+    use exonum::storage::{Fork, ListIndex, ProofMapIndex, Snapshot, ValueSetIndex};
+    use exonum::blockchain::gen_prefix;
+    use exonum::crypto::{Hash, PublicKey};
+
+    /// Schema of the key-value storage used by the cryptoowls service.
+    pub struct CryptoOwlsSchema<T> {
+        view: T,
+    }
+
+    impl<T> CryptoOwlsSchema<T>
+    where
+        T: AsRef<Snapshot>,
+    {
+        pub fn new(view: T) -> Self {
+            CryptoOwlsSchema { view }
+        }
+
+        pub fn users_proof(&self) -> ProofMapIndex<&T, PublicKey, User> {
+            ProofMapIndex::new("cryptoowls.users", &self.view)
+        }
+
+        pub fn owls_state_proof(&self) -> ProofMapIndex<&T, Hash, CryptoOwlState> {
+            ProofMapIndex::new("cryptoowls.owls_state", &self.view)
+        }
+
+        pub fn orders_proof(&self) -> ProofMapIndex<&T, Hash, Order> {
+            ProofMapIndex::new("cryptoowls.orders", &self.view)
+        }
+
+        pub fn users_owls_view(&self, public_key: &PublicKey) -> ValueSetIndex<&T, Hash> {
+            ValueSetIndex::with_prefix("cryptoowls.users_owls", gen_prefix(public_key), &self.view)
+        }
+
+        pub fn user_orders_view(&self, public_key: &PublicKey) -> ListIndex<&T, Hash> {
+            ListIndex::with_prefix("cryptoowls.user_orders", gen_prefix(public_key), &self.view)
+        }
+
+        pub fn owl_orders_view(&self, owl_id: &Hash) -> ListIndex<&T, Hash> {
+            ListIndex::with_prefix("cryptoowls.owl_orders", gen_prefix(owl_id), &self.view)
+        }
+    }
+
+    impl<'a> CryptoOwlsSchema<&'a mut Fork> {
+        pub fn users(&mut self) -> ProofMapIndex<&mut Fork, PublicKey, User> {
+            ProofMapIndex::new("cryptoowls.users", self.view)
+        }
+
+        pub fn owls_state(&mut self) -> ProofMapIndex<&mut Fork, Hash, CryptoOwlState> {
+            ProofMapIndex::new("cryptoowls.owls_state", self.view)
+        }
+
+        pub fn orders(&mut self) -> ProofMapIndex<&mut Fork, Hash, Order> {
+            ProofMapIndex::new("cryptoowls.orders", self.view)
+        }
+
+        pub fn users_owls(&mut self, public_key: &PublicKey) -> ValueSetIndex<&mut Fork, Hash> {
+            ValueSetIndex::with_prefix("cryptoowls.users_owls", gen_prefix(public_key), self.view)
+        }
+
+        pub fn user_orders(&mut self, public_key: &PublicKey) -> ListIndex<&mut Fork, Hash> {
+            ListIndex::with_prefix("cryptoowls.user_orders", gen_prefix(public_key), self.view)
+        }
+
+        pub fn owl_orders(&mut self, owl_id: &Hash) -> ListIndex<&mut Fork, Hash> {
+            ListIndex::with_prefix("cryptoowls.owl_orders", gen_prefix(owl_id), self.view)
+        }
+    }
+}
 /// Модуль с описанием транзакций для демки.
 pub mod transactions {
     use exonum::crypto::{CryptoHash, Hash, PublicKey};
@@ -724,5 +793,4 @@ pub mod service {
             Some(Box::new(router))
         }
     }
-
 }
