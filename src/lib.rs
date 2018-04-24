dec6465670cd705cf8af4655777a52b4989d4eb0
Aleksei Sidorov <aleksei.sidorov@xdev.re>
2018-04-24 14:29:58 +0300

    Silent `let_and_return` clippy warning

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// FIXME: Sometimes clippy incorrectly calculates lifetimes.
+#![cfg_attr(feature="cargo-clippy", allow(let_and_return))]
+
 #[macro_use]
 extern crate display_derive;
 #[macro_use]
